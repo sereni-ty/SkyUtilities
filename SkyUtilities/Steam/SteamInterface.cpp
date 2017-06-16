c60add23795cada9319ab74371ed78d6f8263dd8
sereni-ty <serni_ty@web.de>
2017-06-16 20:53:27 +0200

    Steam: Fixed lagging. Achievements now buffered at startup. Functions will be called immediately.

--- SkyUtilities/Steam/SteamInterface.cpp
+++ SkyUtilities/Steam/SteamInterface.cpp
@@ -6,6 +6,9 @@
 #include "SKSE/PapyrusNativeFunctions.h"
 
 #include <string>
+#include <mutex>
+#include <thread>
+#include <future>
 
 namespace SKU::Steam::Config {
   Configuration::Setting<bool> SteamAPIEnabled {
@@ -18,8 +21,30 @@ namespace SKU::Steam::Config {
   };
 }
 
-namespace SKU::Steam {
+namespace SKU::Steam { // TODO: achievements buffern..
   const long InvalidAchievementID = -1;
+
+  struct Achievement
+  {
+    std::string name, description;
+    bool status;
+
+    Achievement()
+      : name(""), description(""), status(false)
+    {}
+
+    Achievement(std::string n, std::string d, bool s)
+      : name(n), description(d), status(s)
+    {
+      Plugin::Log(LOGL_VERBOSE, "Achievement: %s ('%s'): %s", n.c_str(), d.c_str(), s ? "done" : "undone");
+    }
+  };
+
+  std::vector<Achievement> achievements;
+  std::map<std::string, long> achievement_indices;
+
+  std::mutex achievement_retrieval_mtx;
+  std::future<long> achievement_retrieval_startup_future;
 
   inline bool IsSteamAPIAccessible(SteamAPI &api)
   {
@@ -35,11 +60,48 @@ namespace SKU::Steam {
     return false;
   }
 
+  inline long RetrieveAchievements()
+  {
+    std::unique_lock<std::mutex> scope_lock(achievement_retrieval_mtx);
+
+    if (achievements.empty() == false)
+    {
+      return achievements.size();
+    }
+
+    SteamAPI::DeployAppIDFile();
+    SteamAPI api;
+
+    if (IsSteamAPIAccessible(api) == false)
+    {
+      return -1;
+    }
+
+    size_t num_achievements = api.GetStats()->GetNumAchievements();
+    achievements.resize(num_achievements);
+
+    Plugin::Log(LOGL_INFO, "Steam: %d achievements..", achievements.capacity());
+
+    for (size_t i = 0; i < num_achievements; i++)
+    {
+      std::string name = api.GetStats()->GetAchievementName(i);
+      std::string description = api.GetStats()->GetAchievementDisplayAttribute(name.c_str(), "desc");
+
+      bool status = false;
+      api.GetStats()->GetAchievement(name.c_str(), &status);
+
+      achievement_indices[name] = i;
+      achievements[i] = Achievement(name, description, status);
+    }
+
+    return achievements.capacity();
+  }
+
   Interface::Interface()
   {
     Plugin::GetInstance()->GetConfiguration()->SetInitial(Config::SteamAPIEnabled);
 
-    SteamAPI::DeployAppIDFile();
+    achievement_retrieval_startup_future = std::move(std::async(RetrieveAchievements));
   }
 
   Interface::~Interface()
@@ -71,16 +133,14 @@ namespace SKU::Steam {
 
   long Interface::GetNextAchievementID(StaticFunctionTag*, long i)
   {
-    SteamAPI api;
-
-    if (IsSteamAPIAccessible(api) == false)
+    if (RetrieveAchievements() <= 0)
     {
       return InvalidAchievementID;
     }
 
     i = (i < 0) ? 0 : 1 + i;
 
-    if (api.GetStats()->GetNumAchievements() <= i)
+    if (achievements.size() <= i)
     {
       return InvalidAchievementID;
     }
@@ -90,46 +150,46 @@ namespace SKU::Steam {
 
   BSFixedString Interface::GetAchievementName(StaticFunctionTag*, long i)
   {
-    SteamAPI api;
-
-    if (IsSteamAPIAccessible(api) == false)
+    if (RetrieveAchievements() <= 0 || achievements.size() <= i || i < 0)
     {
       return "";
     }
 
-    if (api.GetStats()->GetNumAchievements() <= i)
-    {
-      return "";
-    }
-
-    return api.GetStats()->GetAchievementName(i); // TODO: Check if that's okay if a nullptr is returned
+    return achievements[i].name.c_str();
   }
 
   bool Interface::GetAchievementStatus(StaticFunctionTag*, BSFixedString name)
   {
-    SteamAPI api;
-
-    if (IsSteamAPIAccessible(api) == false)
+    if (RetrieveAchievements() <= 0)
     {
       return InvalidAchievementID;
     }
 
-    bool status = false;
-    api.GetStats()->GetAchievement(name.data, &status);
+    auto achievement_index = achievement_indices.find(name.data);
 
-    return status;
+    if (achievement_index != achievement_indices.end())
+    {
+      return achievements[(*achievement_index).second].status;
+    }
+
+    return InvalidAchievementID;
   }
 
   BSFixedString Interface::GetAchievementDescription(StaticFunctionTag*, BSFixedString name)
   {
-    SteamAPI api;
-
-    if (IsSteamAPIAccessible(api) == false)
+    if (RetrieveAchievements() <= 0)
     {
       return "";
     }
 
-    return api.GetStats()->GetAchievementDisplayAttribute(name.data, "desc");
+    auto achievement_index = achievement_indices.find(name.data);
+
+    if (achievement_index != achievement_indices.end())
+    {
+      return achievements[(*achievement_index).second].description.c_str();
+    }
+
+    return "";
   }
 
   void Interface::OnSKSERegisterPapyrusFunctions(VMClassRegistry *registry) noexcept
@@ -141,6 +201,11 @@ namespace SKU::Steam {
     registry->RegisterFunction(new NativeFunction1<StaticFunctionTag, BSFixedString, long>("GetAchievementName", "SKUSteam", GetAchievementName, registry));
     registry->RegisterFunction(new NativeFunction1<StaticFunctionTag, bool, BSFixedString>("GetAchievementStatus", "SKUSteam", GetAchievementStatus, registry));
     registry->RegisterFunction(new NativeFunction1<StaticFunctionTag, BSFixedString, BSFixedString>("GetAchievementDescription", "SKUSteam", GetAchievementDescription, registry));
+
+    registry->SetFunctionFlags("SKUSteam", "GetNextAchievementID", VMClassRegistry::kFunctionFlag_NoWait);
+    registry->SetFunctionFlags("SKUSteam", "GetAchievementName", VMClassRegistry::kFunctionFlag_NoWait);
+    registry->SetFunctionFlags("SKUSteam", "GetAchievementStatus", VMClassRegistry::kFunctionFlag_NoWait);
+    registry->SetFunctionFlags("SKUSteam", "GetAchievementDescription", VMClassRegistry::kFunctionFlag_NoWait);
 
     Plugin::Log(LOGL_DETAILED, "Steam: Registered Papyrus functions.");
   }
